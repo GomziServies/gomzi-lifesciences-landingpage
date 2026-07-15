c0a12e2e143ff6d86bedd6aa96acf3e4ca6d4d69
Yash Sutariya <84495766+yash-sutariya@users.noreply.github.com>
2026-07-15 12:16:11 +0530

    feat: add apiConfig module to manage environment-specific base URLs and merchant IDs

--- src/assets/js/config/apiConfig.js
+++ src/assets/js/config/apiConfig.js
@@ -3,16 +3,16 @@ let baseUrl = "http://localhost";
 let razorpayMerchantId = "rzp_test_F0TUZmabOwKkhe";
 
 if (
-  hostname === "purego.gomzilifesciences.in" ||
-  hostname === "www.purego.gomzilifesciences.in"
+  hostname === "glssamplebox.gomzilifesciences.in" ||
+  hostname === "www.glssamplebox.gomzilifesciences.in"
 ) {
   baseUrl = "https://api.fggroup.in";
-  razorpayMerchantId = "rzp_live_tdfTCMm8C9gJNN";
+  razorpayMerchantId = "rzp_live_eG55rec1KDFHm7";
 } else if (hostname === "test.purego.gomzilifesciences.in") {
   baseUrl = "https://dev-api.fggroup.in";
 } else {
-//   baseUrl = "https://dev-api.fggroup.in";
-  baseUrl = "http://localhost:80";
+  baseUrl = "https://dev-api.fggroup.in";
+  // baseUrl = "http://localhost:80";
 }
 
 const apiConfig = {
